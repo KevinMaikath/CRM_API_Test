65d164b9fd01f6c11c0b57d55f23104471ccd183
Kevin <kevinrx25@gmail.com>
2019-12-29 20:21:59 +0000

    Image GET request

--- CRM_API/urls.py
+++ CRM_API/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from CRM_API.views import CustomerList, CustomerDetail, UserCreate, LoginView
 from django.urls import path
 
@@ -7,3 +10,5 @@ urlpatterns = [
     path('users', UserCreate.as_view(), name='user_create'),
     path('login', LoginView.as_view(), name='login')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
